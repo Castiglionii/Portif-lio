602a295894b9ee9ee7cbabf144bd1d3017830d88
Castiglionii <viniciusevangelista14@hotmail.com>
2026-05-23 23:47:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+let tocando = false;
+
 function tocarSom() {
 
+  if (tocando) return;
+
   const som = document.getElementById("somMascote");
+
+  tocando = true;
 
   som.volume = 0.3;
 
   som.currentTime = 0;
 
   som.play();
+
+  som.onended = () => {
+    tocando = false;
+  };
 
 }
